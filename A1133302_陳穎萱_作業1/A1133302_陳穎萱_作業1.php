8f4b25dd506061456ec77cf032550e9dc8a412dc
a1133302 <a1133302@mail.nuk.edu.tw>
2026-03-25 22:16:01 +0800

    Add files via upload

---
+++ A1133302_陳穎萱_作業1/A1133302_陳穎萱_作業1.php
@@ -0,0 +1,94 @@
+<html>
+
+<head>
+
+<title>夏令營報名表</title>
+
+</head>
+
+<body>
+<h1><center>2026 高大夏令營</center></h1>
+<hr color="blue"/>
+<font size="5" color="brown"><center>招收對象6-12歲孩童</center></font>
+<h3><a href="#報名">我要報名</a></h3>
+<h2><i>7/10~7/12課程規劃</i></h2>
+</br>
+<h3>繪畫夏令營</h3>
+<img src="R.jpg" width="20%" height="20%"></br>
+來源:https://xbeibeix.com/video/BV1es411e7H5
+<table border="2" bgcolor="white" width="30%" height="30%">
+<tr bgcolor="white"><th>日期</th><th>時間</th><th>課程</th><th>費用</th><th>上課地點</th></tr>
+<tr><td>7/10</td><td>15:00~17:00</td><td>水墨畫</td><td>1000</td><td bgcolor="pink">綜合大樓107</td></tr>
+<tr><td>7/11</td><td>15:00~17:00</td><td>油畫</td><td>2000</td><td bgcolor="yellow">管理學院201</td></tr>
+<tr><td>7/12</td><td >15:00~17:00</td><td>水彩畫</td><td>1500</td><td bgcolor="pink">綜合大樓104</td></tr>
+</table>
+</br>
+<h2><i>7/15~7/17課程規劃</i></h2>
+</br>
+<h3>資訊夏令營</h3>
+<img src="Python-Logo.png" width="20%" height="20%"></br>
+來源:https://logos-world.net/python-logo/
+<table border="2" bgcolor="white" width="30%" height="30%">
+<tr bgcolor="white"><th>日期</th><th>時間</th><th>課程</th><th>費用</th><th>上課地點</th></tr>
+<tr><td>7/10</td><td>14:00~16:30</td><td>計算機概論</td><td>1000</td><td bgcolor="orange">圖書資訊大樓107</td></tr>
+<tr><td>7/11</td><td>14:00~16:30</td><td>python簡介</td><td>2000</td><td bgcolor="pink">綜合大樓104</td></tr>
+<tr><td>7/12</td><td >14:00~16:30</td><td>python應用</td><td>2000</td><td bgcolor="orange">圖書資訊大樓108</td></tr>
+</table>
+</br>
+
+<h3><font size="" color="red">注意事項:</font></h3>
+<ul>
+<li>需繳預付500元(參加完之後會退費)
+<li>如需取消，需在兩個星期之前，否則無法拿回預繳金額
+<li>如若當天無法準時參加課程，須提前告知
+<li>請在參加的一週前匯款
+<li><font size="" color="blue">匯款帳戶:12345678</font>
+</ul>
+
+
+<hr color="black"/>
+<h2><center><a name="報名">報名資訊</a></center></h2>
+
+
+
+<form action="" method="">
+<select name="nActivity">
+<option value="drawing">繪畫夏令營<option>
+<option value="Programming">資訊夏令營<option>
+</select>
+</br></br>
+姓名: <input type="text" placeholder="your name" name="nName" value=""></br>
+性別:</br>
+男<input type="radio" name="mGender" value="f" checked>
+女<input type="radio" name="mGender" value="m"></br>
+收到高大接下來的廣告與通知:</br>
+願意<input type="checkbox" name="mYes" value="yes"> 
+不願意<input type="checkbox" name="mNo" value="no"></br>
+參加的日期:<input type="date" name="mDate" value="" ></br>
+<h4>聯絡方式:</h4>
+電話:<input type="phoneNumber"  placeholder="your phone number" name="nphoneNumber"></br>
+信箱:<input type="email" name="mEmail" value="" ></br>
+年齡:<input type="number" name="mage" value="" ></br>
+<h4>其他建議:<h4>
+<textarea name="comment" rows="10" cols="30"></textarea>
+</br>
+<input type="submit">
+<input type="reset">
+
+</form>
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
